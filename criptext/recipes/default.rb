76b06602a133c3095a168b00f926d564d3009527
Gianni Carlo <gcarlo89@hotmail.com>
2016-10-27 15:42:35 -0500

    add dependencies

--- criptext/recipes/default.rb
+++ criptext/recipes/default.rb
@@ -1,6 +1,6 @@
 package 'git'
 
 git "/home/ec2-user/repo1" do
-  repository https://github.com/GianniCarlo/application_git
+  repository https://github.com/criptext/opsworks-recipes
   action :checkout
 end
