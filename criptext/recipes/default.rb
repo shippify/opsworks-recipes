01f4bd8d91c15723423d304b89b51d158b3432d2
Gianni Carlo <gcarlo89@hotmail.com>
2016-10-27 16:09:49 -0500

    test2

--- criptext/recipes/default.rb
+++ criptext/recipes/default.rb
@@ -1,13 +1,5 @@
 package 'git'
-package 'application'
-package 'application_git'
 
-application '/home/ec2-user/repo1' do
-  # Application resource properties.
-  owner 'root'
-  group 'root'
-
-  git '/home/ec2-user/repo' do
-    repository 'https://github.com/criptext/opsworks-recipes'
-  end
+git '/home/ec2-user/repo' do
+  repository 'https://github.com/criptext/opsworks-recipes'
 end
