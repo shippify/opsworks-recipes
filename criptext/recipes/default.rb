1daa98e5b085c7dad43e401e00cc39d13474ab10
Gianni Carlo <gcarlo89@hotmail.com>
2016-10-27 16:45:58 -0500

    test4

--- criptext/recipes/default.rb
+++ criptext/recipes/default.rb
@@ -1,5 +1,12 @@
-package 'git'
+#package 'git'
 
-git '/home/ec2-user/monkey-web' do
-  repository 'https://github.com/Criptext/Monkey-Web-API'
+#git '/home/ec2-user/monkey-web' do
+#  repository 'https://github.com/Criptext/Monkey-Web-API'
+#end
+
+package 'application_git'
+
+application_git '/home/ec2-user/monkey_web' do
+  repository 'git@github.com:Criptext/Monkey-Web-API.git'
+  deploy_key chef_vault_item('deploy_keys', 'monkey_web')['key']
 end
