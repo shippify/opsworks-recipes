e629d0e18e54a6a521f42a4b9e0bda95191e5b6e
Gianni Carlo <gianni@criptext.com>
2016-11-09 17:58:41 -0500

    make sure files exist

--- monkey_web/recipes/default.rb
+++ monkey_web/recipes/default.rb
@@ -1,28 +1,29 @@
 
+# make sure file app.env exists
+file '/srv/monkey_web/app.env' do
+  content ''
+  action :create
+end
+# make sure file docker-compose.yml exists
+file '/srv/monkey_web/docker-compose.yml' do
+  content ''
+  action :create
+end
+
 # clone repository
 app = search("aws_opsworks_app", "shortname:monkey_web").first
 application_git '/srv/monkey_web' do
   repository app['app_source']['url']
   revision app['app_source']['revision']
   deploy_key app['app_source']['ssh_key']
-  notifies :create, 'file[/srv/monkey_web/app.env]', :immediately
   notifies :create, 'file[/srv/monkey_web/docker-compose.yml]', :immediately
   notifies :create, 'ruby_block[insert_line]', :immediately
 end
 
-# make sure file app.env exists
-file '/srv/monkey_web/app.env' do
-  content ''
-  action :nothing
-end
-
-# create docker-compose file if it doesn't exist
-# necessary 'if' because at compile time docker-compose file doesn't exist
-if File.exist?('/srv/monkey_web/docker-compose-prod.yml')
-  file '/srv/monkey_web/docker-compose.yml' do
+# create docker-compose file
+file '/srv/monkey_web/docker-compose.yml' do
     content IO.read('/srv/monkey_web/docker-compose-prod.yml')
     action :nothing
-  end
 end
 
 # populate app.env file
