0a2b767e52f31b24b0be9ec855117953dc388e89
Gianni Carlo <gianni@criptext.com>
2016-11-09 17:49:33 -0500

    add notifications

--- monkey_web/recipes/default.rb
+++ monkey_web/recipes/default.rb
@@ -5,13 +5,15 @@ application_git '/srv/monkey_web' do
   repository app['app_source']['url']
   revision app['app_source']['revision']
   deploy_key app['app_source']['ssh_key']
-  action :nothing
+  notifies :create, 'file[/srv/monkey_web/app.env]', :immediately
+  notifies :create, 'file[/srv/monkey_web/docker-compose.yml]', :immediately
+  notifies :create, 'ruby_block[insert_line]', :immediately
 end
 
 # make sure file app.env exists
 file '/srv/monkey_web/app.env' do
   content ''
-  action :create
+  action :nothing
 end
 
 # create docker-compose file if it doesn't exist
@@ -19,17 +21,18 @@ end
 if File.exist?('/srv/monkey_web/docker-compose-prod.yml')
   file '/srv/monkey_web/docker-compose.yml' do
     content IO.read('/srv/monkey_web/docker-compose-prod.yml')
-    action :create
+    action :nothing
   end
 end
 
 # populate app.env file
-app['environment'].each do |env_var|
-  ruby_block "insert_line" do
-    block do
+ruby_block "insert_line" do
+  block do
+    app['environment'].each do |env_var|
       file = Chef::Util::FileEdit.new("/srv/monkey_web/app.env")
       file.insert_line_if_no_match("/#{env_var[0]}=#{env_var[1]}/", "#{env_var[0]}=#{env_var[1]}")
       file.write_file
     end
   end
+  action :nothing
 end
