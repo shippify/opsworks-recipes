5d995d7b375b6ad3706dc7aec29421bdd5bb225b
Gianni Carlo <gianni@criptext.com>
2016-11-09 18:32:20 -0500

    add guard statements

--- monkey_web/recipes/default.rb
+++ monkey_web/recipes/default.rb
@@ -1,34 +1,28 @@
 # make sure directory exists
-file '/srv/monkey_web' do
+directory '/srv/monkey_web' do
   action :create
 end
 # make sure file app.env exists
 file '/srv/monkey_web/app.env' do
   content ''
   action :create
+  only_if do ::Dir.exists?('/srv/monkey_web') end
 end
 # make sure file docker-compose.yml exists
 file '/srv/monkey_web/docker-compose.yml' do
   content ''
   action :create
+  only_if do ::Dir.exists?('/srv/monkey_web') end
 end
 
-# clone repository
+# get app
 app = search("aws_opsworks_app", "shortname:monkey_web").first
-application_git '/srv/monkey_web' do
-  repository app['app_source']['url']
-  revision app['app_source']['revision']
-  deploy_key app['app_source']['ssh_key']
-  notifies :create, 'file[/srv/monkey_web/docker-compose.yml]', :immediately
-  notifies :create, 'ruby_block[insert_line]', :immediately
-end
 
 # create docker-compose file
-if File.exist?('/srv/monkey_web/docker-compose-prod.yml')
-  file '/srv/monkey_web/docker-compose.yml' do
-      content IO.read('/srv/monkey_web/docker-compose-prod.yml')
-      action :nothing
-  end
+file '/srv/monkey_web/docker-compose.yml' do
+    content IO.read('/srv/monkey_web/docker-compose-prod.yml')
+    action :nothing
+    only_if do ::File.exists?('/srv/monkey_web/docker-compose-prod.yml') end
 end
 
 # populate app.env file
@@ -41,4 +35,13 @@ ruby_block "insert_line" do
     end
   end
   action :nothing
+end
+
+# clone repository
+application_git '/srv/monkey_web' do
+  repository app['app_source']['url']
+  revision app['app_source']['revision']
+  deploy_key app['app_source']['ssh_key']
+  notifies :create, 'file[/srv/monkey_web/docker-compose.yml]', :immediately
+  notifies :create, 'ruby_block[insert_line]', :immediately
 end
