35a88fbdbeafb55f8fa93bd9f8a94518ba5e0852
Gianni Carlo <gcarlo89@hotmail.com>
2016-12-06 16:03:19 -0500

    Add deregister recipe

--- alb/recipes/default.rb
+++ alb/recipes/default.rb
@@ -1,12 +1,1 @@
-
-instance = search("aws_opsworks_instance", "self:true").first
-
-Chef::Log.info("********** group arn '#{node['target-group-arn']}' **********")
-Chef::Log.info("********** id instance '#{instance['ec2_instance_id']}' **********")
-Chef::Log.info("********** region '#{instance['region']}' **********")
-
-bash 'register_balancer' do
-  code <<-EOH
-  aws elbv2 register-targets --target-group-arn #{node['target-group-arn']} --targets Id=#{instance['ec2_instance_id']} --region #{node['region']}
-  EOH
-end
+include_recipe 'alb::register'
---
+++ alb/recipes/register.rb
@@ -0,0 +1,8 @@
+
+instance = search("aws_opsworks_instance", "self:true").first
+
+bash 'register_balancer' do
+  code <<-EOH
+  aws elbv2 register-targets --target-group-arn #{node['target-group-arn']} --targets Id=#{instance['ec2_instance_id']} --region #{node['region']}
+  EOH
+end
