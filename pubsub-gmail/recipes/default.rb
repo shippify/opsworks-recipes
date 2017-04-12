1607093289810c87165999709ee9d9b87281e2af
Erika Perugachi <erikaperugachi@gmail.com>
2017-04-12 13:45:08 -0500

    Update YUM with Cloud SDK repo information

--- pubsub-gmail/recipes/default.rb
+++ pubsub-gmail/recipes/default.rb
@@ -1,5 +1,17 @@
 bash 'run_command' do
   code <<-EOH
+	  
+	  sudo tee -a /etc/yum.repos.d/google-cloud-sdk.repo << EOM
+		[google-cloud-sdk]
+		name=Google Cloud SDK
+		baseurl=https://packages.cloud.google.com/yum/repos/cloud-sdk-el7-x86_64
+		enabled=1
+		gpgcheck=1
+		repo_gpgcheck=1
+		gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
+		       https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+		EOM	  
+	  
 		yum install google-cloud-sdk && gcloud init
   EOH
 end
