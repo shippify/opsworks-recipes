301974e46c01783cadd4d35e09872cc53091c399
Gianni Carlo <gcarlo89@hotmail.com>
2017-08-18 17:49:44 -0500

    Add directive to include submodules on clone

--- opsworks/recipes/deploy.rb
+++ opsworks/recipes/deploy.rb
@@ -107,5 +107,6 @@ application_git "/srv/#{node['app']}" do
   repository app['app_source']['url']
   revision app['app_source']['revision']
   deploy_key app['app_source']['ssh_key']
+  enable_submodules true
   notifies :create, "file[/srv/#{node['app']}/app.env]", :immediately
 end
--- opsworks/recipes/pull.rb
+++ opsworks/recipes/pull.rb
@@ -21,5 +21,6 @@ application_git "/srv/#{node['app']}" do
   repository app['app_source']['url']
   revision app['app_source']['revision']
   deploy_key app['app_source']['ssh_key']
+  enable_submodules true
   notifies :create, "ruby_block[insert_line]", :immediately
 end
