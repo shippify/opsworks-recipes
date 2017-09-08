3114f04270cc916c1bed38053697cbcb96e2a0c8
Gianni Carlo <gcarlo89@hotmail.com>
2017-09-08 12:15:57 -0500

    Add nil check on opsworks::copy

--- opsworks/recipes/copy.rb
+++ opsworks/recipes/copy.rb
@@ -1,8 +1,10 @@
 # copy files
 ruby_block "copy_files" do
   block do
-    node['copy-files'].each do |file_var|
-      FileUtils::cp "#{file_var['source']}", "/#{file_var['destination']}"
+    unless node['copy-files'].nil?
+      node['copy-files'].each do |file_var|
+        FileUtils::cp "#{file_var['source']}", "/#{file_var['destination']}"
+      end
     end
   end
   action :create
