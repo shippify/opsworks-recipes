fda55c8461f3919c115b3dde56cccd9b55f7a519
Gianni Carlo <gcarlo89@hotmail.com>
2017-09-08 01:03:44 -0500

    Change copy recipe again

--- opsworks/recipes/copy.rb
+++ opsworks/recipes/copy.rb
@@ -2,7 +2,7 @@
 ruby_block "copy_files" do
   block do
     node['copy-files'].each do |file_var|
-      cp file_var['source'] file_var['destination']
+      FileUtils::cp "#{file_var['source']}", "/#{file_var['destination']}"
     end
   end
   action :create
