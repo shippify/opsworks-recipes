122a2333235580b0e4a037c6476be5bd367ce00a
Daniel Tigse <daniel@criptext.com>
2018-08-02 00:13:03 -0500

    Update pull_only.rb

--- opsworks/recipes/pull_only.rb
+++ opsworks/recipes/pull_only.rb
@@ -2,6 +2,9 @@ unless node['app']
   Chef::Application.fatal!("There is no app specified in the custom JSON.")
 end
 
+# get app
+app = search("aws_opsworks_app", "shortname:#{node['app']}").first
+
 # clone repository
 application_git "/srv/#{node['app']}" do
   repository app['app_source']['url']
