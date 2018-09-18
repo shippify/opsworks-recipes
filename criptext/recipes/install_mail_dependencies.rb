dae973e93440eddc34cd756ccd585c8d759e82d3
Gabriel Aumala <gabrielfima@live.com>
2018-09-18 14:58:05 -0500

    install node and yarn from RPM

--- criptext/recipes/install_mail_dependencies.rb
+++ criptext/recipes/install_mail_dependencies.rb
@@ -1,3 +1,11 @@
+remote_file '/root/node_install.sh' do
+  source 'https://rpm.nodesource.com/setup_8.x'
+end
+
+bash 'install node from RPM' do
+  code 'bash /root/node_install.sh'
+end
+
 remote_file '/root/nvm_install.sh' do
   source 'https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh'
 end
@@ -10,9 +18,8 @@ bash 'install node via nvm' do
   EOH
 end
 
-
 remote_file '/etc/yum.repos.d/yarn.repo' do
   source 'https://dl.yarnpkg.com/rpm/yarn.repo'
 end
 
-package "yarn"
+package 'yarn' 
