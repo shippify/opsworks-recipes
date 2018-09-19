21abe42c31d2024f9e90212cbfd0c202bb341447
Daniel Tigse <danieltp15@hotmail.com>
2018-09-19 11:48:51 -0500

    rename recipe

--- criptext/recipes/pull_and_restart.rb
+++ criptext/recipes/pull_and_restart.rb
@@ -1,11 +1,16 @@
 
 app = search("aws_opsworks_app", "shortname:#{node['app']}").first
 application_git "/srv/#{node['app']}" do
-  Chef::Log.debug("with url: #{app['app_source']['url']}")
   repository app['app_source']['url']
-  Chef::Log.debug("with ssh_key /srv/#{app['app_source']['ssh_key']}")
   deploy_key app['app_source']['ssh_key']
 end
+
+bash 'yarn install' do
+    code <<-EOH
+      cd /srv/keyserver
+      yarn --frozen-lockfile
+    EOH
+  end
 
 bash 'restart_supervisor' do
   code <<-EOH
