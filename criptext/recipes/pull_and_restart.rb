9be60ec4263fedceb9c6fa0bb53f29cb3a06771d
Daniel Tigse <danieltp15@hotmail.com>
2018-09-19 14:34:52 -0500

    exporting env var

--- criptext/recipes/pull_and_restart.rb
+++ criptext/recipes/pull_and_restart.rb
@@ -1,10 +1,21 @@
 
+#clone repository
 app = search("aws_opsworks_app", "shortname:#{node['app']}").first
 application_git "/srv/#{node['app']}" do
   repository app['app_source']['url']
   deploy_key app['app_source']['ssh_key']
 end
 
+#export environment variables
+ruby_block "export_vars" do
+  block do
+    app['environment'].each do |env_var|
+      ENV["#{env_var[0]}"] = env_var[1]
+    end
+  end
+end
+
+#install dependencies
 bash 'yarn install' do
   cwd '/srv/keyserver'
   code <<-EOH
@@ -12,6 +23,7 @@ bash 'yarn install' do
     EOH
   end
 
+#restart supervisor
 bash 'restart_supervisor' do
   code <<-EOH
     cp /srv/keyserver/supervisor.conf /etc/supervisor/conf.d/api_server.conf
