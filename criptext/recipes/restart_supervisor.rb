ef4802320ae882b59c69998f5db0417f9d62cdfa
Daniel Tigse <danieltp15@hotmail.com>
2018-09-19 11:19:32 -0500

    logs for testing

--- criptext/recipes/restart_supervisor.rb
+++ criptext/recipes/restart_supervisor.rb
@@ -1,6 +1,5 @@
 bash 'restart_supervisor' do
   code <<-EOH
-    mkdir -p /etc/supervisor/conf.d
     cp /srv/keyserver/supervisor.conf /etc/supervisor/conf.d/api_server.conf
     service supervisord restart
   EOH
--- opsworks/recipes/pull_only.rb
+++ opsworks/recipes/pull_only.rb
@@ -7,8 +7,12 @@ app = search("aws_opsworks_app", "shortname:#{node['app']}").first
 
 # clone repository
 application_git "/srv/#{node['app']}" do
+  Chef::Log.debug("Clonning repository into /srv/#{node['app']}")
+  Chef::Log.debug("with url: #{app['app_source']['url']}")
   repository app['app_source']['url']
+  Chef::Log.debug("with revision: #{app['app_source']['revision']}")
   revision app['app_source']['revision']
+  Chef::Log.debug("with ssh_key /srv/#{app['app_source']['ssh_key']}")
   deploy_key app['app_source']['ssh_key']
   enable_submodules true
   action :nothing
